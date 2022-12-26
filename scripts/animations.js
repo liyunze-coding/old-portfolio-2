a761f83183932efe7f230a7cf5743158ca89a20e
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2022-12-26 20:15:04 +0800

    project video pauses when not visible

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,9 +1,9 @@
 const scroll_observer = new IntersectionObserver((entries) => {
     entries.forEach( (entry) => {
         if (entry.isIntersecting){
-            entry.target.classList.add('show-slide')
+            entry.target.classList.add('show-slide');
         } else {
-            entry.target.classList.remove('show-slide')
+            entry.target.classList.remove('show-slide');
         }
     })
 })
@@ -13,7 +13,17 @@ const fade_observer = new IntersectionObserver((entries) => {
         if (entry.isIntersecting){
             entry.target.classList.add('show-fade');
         } else {
-            entry.target.classList.remove('show-fade')
+            entry.target.classList.remove('show-fade');
+        }
+    })
+})
+
+const projectIconObserver = new IntersectionObserver((entries) => {
+    entries.forEach( (entry) => {
+        if (entry.isIntersecting){
+            entry.target.play();
+        } else {
+            entry.target.pause();
         }
     })
 })
@@ -21,6 +31,9 @@ const fade_observer = new IntersectionObserver((entries) => {
 $(window).on("load", async function () {
     const hiddenScrolls = document.querySelectorAll('.hidden-slide');
     const hiddenFades = document.querySelectorAll('.hidden-fade');
+    const projectPlay = document.querySelectorAll('#project-icon');
+
     hiddenScrolls.forEach((el) => scroll_observer.observe(el));
     hiddenFades.forEach((el) => fade_observer.observe(el));
+    projectPlay.forEach((el) => projectIconObserver.observe(el));
 });
